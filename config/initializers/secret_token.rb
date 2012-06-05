4b43a1c063ac19b8ddc633cd203c9814690394f2
Tymon Tobolski <i@teamon.eu>
2012-06-05 17:44:36 +0200

    app init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hal::Application.config.secret_token = '562607219bd44e65ddf6ab118d9004aa2e3d74781f1e93c62fd2e0878ce9e003cad8b7315c0eed3cca159848ba785ada99fbe95c525c3476e8312e7fe68356cc'
