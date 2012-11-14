32de7ade3fcde2f16f66a77819a16dd95ab358b0
Tymon Tobolski <i@teamon.eu>
2012-11-14 19:10:46 +0100

    Prevent access for regular users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  BLOCKED_ACTIONS = {
+    "categories"  => ["new", "create", "edit", "update", "destroy"],
+    "items"       => ["new", "create", "edit", "update", "destroy"],
+    "orders"      => ["index", "edit", "update", "accept", "reject"],
+    "users"       => ["new", "create", "edit", "update", "destroy"],
+    "stuffs"      => ["new", "create", "edit", "update", "destroy"]
+  }
+
+  def prevent_access
+    unless current_user.admin?
+      actions = BLOCKED_ACTIONS[params[:controller].to_s]
+      if actions && actions.include?(params[:action].to_s)
+        redirect_to "/" and return false
+      end
+    end
+  end
 end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -109,7 +109,7 @@ ActiveAdmin.setup do |config|
   # You can add before, after and around filters to all of your
   # Active Admin resources from here.
   #
-  # config.before_filter :do_something_awesome
+  config.before_filter :prevent_access
 
 
   # == Register Stylesheets & Javascripts
